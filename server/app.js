3ed85566511c488f7f6a85119cc41db159ba70cc
Artur Ptaszek <artur.ptaszek@gmail.com>
2015-04-28 18:46:50 +0200

    boilerplate changed

--- server/app.js
+++ server/app.js
@@ -1,18 +1,32 @@
+/**
+ * Main application file
+ */
+
+'use strict';
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var config = require('./config/environment');
 
-var routes = require('./routes/index');
+// Connect to database
+mongoose.connect(config.mongo.uri, config.mongo.options);
 
+// Populate DB with sample data
+if(config.seedDB) { require('./config/seed'); }
+
+// Setup server
 var app = express();
+var server = require('http').createServer(app);
+require('./config/express')(app);
+require('./routes')(app);
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Start server
+server.listen(config.port, config.ip, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
 
-app.use('/', routes);
-
-module.exports = app;
+// Expose app
+exports = module.exports = app;
