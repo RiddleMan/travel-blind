f4fa34a0b7d9c40a0644f075e3bd8466bfbe56d5
Artur Ptaszek <artur.ptaszek@gmail.com>
2015-05-28 22:07:18 +0200

    closes #2 created text to speech

--- server/api/route/route.controller.js
+++ server/api/route/route.controller.js
@@ -3,41 +3,31 @@
 var _ = require('lodash');
 var config = require('../../config/environment');
 var maps = require('../../components/mapsConnector')(config.maps);
-var xml2js = require('xml2js');
+var mapper = require('./gMapsMapper');
 
-// Get connection from point A to B
-function buildResponse(directionsRes) {
-  var tmp = directionsRes.DirectionsResponse.route[0].leg[0];
-  var res = {};
+function xml(res, payload, status) {
+  if(status)
+    res.status(status);
 
-  res.departure = tmp.departure_time[0].text;
-  res.arrival = tmp.arrival_time[0].text;
-  res.distance = tmp.distance[0].text;
-  res.steps = {
-    step: tmp.step.filter((step) => step.travel_mode[0] === 'TRANSIT')
-      .map((step) => {
-        var tmp = step.transit_details[0];
-        return {
-          from: tmp.departure_stop[0].name[0],
-          to: tmp.arrival_stop[0].name[0],
-          stopsCount: tmp.num_stops[0],
-          departure: tmp.departure_time[0].text[0],
-          arrival: tmp.arrival_time[0].text[0],
-          line: (tmp.line[0].short_name || tmp.line[0].name)[0]
-        };
-    })
-  }
-
-  return res;
-}
-
-function buildXml(json) {
-
-  var builder = new xml2js.Builder();
-  return builder.buildObject(json);
+  res.set('Content-Type', 'application/xml');
+  res.send(payload);
 }
 
 module.exports.index = function(req, res) {
+  if(!req.query.origin)
+    xml(res, `
+      <xml>
+        <error>Origin parameter is required</error>
+      </xml>
+      `, 400);
+
+  if(!req.query.destination)
+    xml(res, `
+      <xml>
+        <error>Destination parameter is required</error>
+      </xml>
+      `, 400);
+
   maps.directions.get({
       origin: req.query.origin,
       destination: req.query.destination,
@@ -45,16 +35,13 @@ module.exports.index = function(req, res) {
       units: 'metric'
     })
     .then((response) => {
-      res.set('Content-Type', 'application/xml');
-      var resp = buildResponse(response);
-      var xml = buildXml(resp);
-      res.send(xml);
+      xml(res, mapper(response));
 
-    }, (err) => res.json(403, err))
-    .catch((err) =>
-      console.dir(err));
+    }, (err) => xml(res, `<xml>
+        <error>${err}</error>
+      </xml>`, 403))
+    .catch((err) => xml(res, `
+      <xml>
+        <error>Unexpected server error</error>
+      </xml>`, 500));
 };
-
-function handleError(res, err) {
-  return res.send(500, err);
-}
--- server/app.js
+++ server/app.js
@@ -3,9 +3,7 @@
  */
 
 'use strict';
-require("babel/register")({
-    sourceMap: 'both'
-});
+require("babel/register");
 
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
